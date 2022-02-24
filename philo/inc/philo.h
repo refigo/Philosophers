49ca0fff79b064d795a3f19c868fef650732c869
refigo <wlguq100@naver.com>
2022-02-24 11:52:07 +0900

    set Makefile

---
+++ philo/inc/philo.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 09:07:25 by mgo               #+#    #+#             */
+/*   Updated: 2022/02/24 10:32:17 by mgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+
+struct s_philo
+{
+	int	num_name;
+
+}		t_philo;
+
+struct s_setting
+{
+	int	num_of_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_of_times_each_must_eat;
+
+}		t_setting;
+
+#endif
