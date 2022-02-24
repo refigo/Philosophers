e525e07111b73ebf3eb96eb8722259779461a657
refigo <wlguq100@naver.com>
2022-02-24 14:27:31 +0900

    feat: set data

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 09:07:25 by mgo               #+#    #+#             */
-/*   Updated: 2022/02/24 13:40:58 by mgo              ###   ########.fr       */
+/*   Updated: 2022/02/24 14:26:56 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,35 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
 
 typedef enum s_bool
 {
-	FALSE,
-	TRUE
+	TRUE,
+	FALSE
 }			t_bool;
 
 typedef struct s_philo
 {
-	int	name;
-
+	int					name;
+	pthread_t			*thread;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+	struct timeval		time_eat_last;
+	struct s_setting	*data;
 }				t_philo;
 
 typedef struct s_setting
 {
-	int	num_of_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_of_times_each_must_eat;
+	int				num_of_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_of_times_each_must_eat;
+	t_philo			*philos;
+	pthread_mutex_t	*forks;
+	struct timeval	time_start;
 
 }				t_setting;
 
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 09:41:00 by mgo               #+#    #+#             */
-/*   Updated: 2022/02/24 13:34:58 by mgo              ###   ########.fr       */
+/*   Updated: 2022/02/24 14:26:37 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,37 @@
 
 
 
-int	return_error_with_msg(char *msg)
+int	error_with_msg(char *msg)
 {
+	mgo_putstr_fd("Error: ", 2);
 	if (msg)
 		mgo_putstr_fd(msg, 2);
-	return (1);
+	mgo_putstr_fd("\n", 2);
+	return (FALSE);
 }
 
-int	get_data(t_setting *data, int argc, char **argv)
+void	get_data(t_setting *data, int argc, char **argv)
 {
+	data->num_of_philos = mgo_atoi(argv[1]);
+	data->time_to_die = mgo_atoi(argv[2]);
+	data->time_to_eat = mgo_atoi(argv[3]);
+	data->time_to_sleep = mgo_atoi(argv[4]);
+	if (argc == 6)
+		data->num_of_times_each_must_eat = mgo_atoi(argv[5]);
+}
 
-	(void)data;
-	(void)argc;
-	(void)argv;
+int	check_data(t_setting *data, int argc)
+{
+	if (data->num_of_philos < 0)
+		return (error_with_msg("number_of_philosophers"));
+	if (data->time_to_die < 0)
+		return (error_with_msg("time_to_die"));
+	if (data->time_to_eat < 0)
+		return (error_with_msg("time_to_eat"));
+	if (data->time_to_sleep < 0)
+		return (error_with_msg("time_to_sleep"));
+	if (argc == 6 && data->num_of_times_each_must_eat <= 0)
+		return (error_with_msg("number_of_times_each_philosopher_must_eat"));
 	return (TRUE);
 }
 
@@ -34,13 +52,11 @@ int	get_data(t_setting *data, int argc, char **argv)
 int	set_data(t_setting *data, int argc, char **argv)
 {
 	memset(data, 0, sizeof(t_setting));
-
 	get_data(data, argc, argv);
-
+	if (check_data(data, argc) == FALSE)
+		return (FALSE);
 
 	test_setting(data);
-	(void)argc;
-	(void)argv;
 	return (TRUE);
 }
 
@@ -49,12 +65,12 @@ int	main(int argc, char **argv)
 	t_setting	data;
 
 	if (argc != 5 && argc != 6)
-		return (1);
-	if (!set_data(&data, argc, argv))
-		return (1);
-	//init_data();
+		return (error_with_msg("the number of args is wrong!"));
+	if (set_data(&data, argc, argv) == FALSE)
+		return (FALSE);
+	//init_for_dining();
 	//have_dining();
 	//clear_data();
 
-	return (0);
+	return (TRUE);
 }
