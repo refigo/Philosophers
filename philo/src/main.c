ac6de9120da8704a112d3cc23f8bcdb5dc2b386a
refigo <wlguq100@naver.com>
2022-03-19 16:17:30 +0900

    config: mute test code

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 09:41:00 by mgo               #+#    #+#             */
-/*   Updated: 2022/03/19 14:56:09 by mgo              ###   ########.fr       */
+/*   Updated: 2022/03/19 15:04:56 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 	if (set_data(&data, argc, argv) == FAIL)
 		return (FAIL);
 	have_dining(&data);
-	test_overall(&data);
+	//test_overall(&data);
 	clear_data(&data);
 	//system("leaks philo");
 	return (SUCCESS);
