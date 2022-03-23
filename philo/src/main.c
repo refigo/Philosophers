5b842ece1ce8d0636e33f1a2c015901fce2a667f
refigo <wlguq100@naver.com>
2022-03-23 14:45:37 +0900

    refact: modify error message of the number of argument

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 09:41:00 by mgo               #+#    #+#             */
-/*   Updated: 2022/03/21 16:36:50 by mgo              ###   ########.fr       */
+/*   Updated: 2022/03/23 14:40:48 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_setting	data;
 
 	if ((argc != 5) && (argc != 6))
-		return (error_with_msg("the number of args wrong!"));
+		return (error_with_msg("the number of argment is wrong!"));
 	if (set_data(&data, argc, argv) == FAIL)
 		return (FAIL);
 	have_dining(&data);
