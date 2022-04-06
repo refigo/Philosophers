f1e28df0a617dc2b683c36035d92d32f01d245f1
refigo <wlguq100@naver.com>
2022-04-06 21:51:53 +0900

    feat: process_philo done roughly

--- philo_bonus/src/have_dining_bonus.c
+++ philo_bonus/src/have_dining_bonus.c
@@ -14,15 +14,53 @@
 #include <unistd.h> // fork()
 #include <stdlib.h> // exit()
 
+void	taking_forks(t_philo *philo)
+{
+	t_setting	*data;
+
+	data = philo->data;
+	sem_wait(data->forks);
+	print_philo_status(philo, "has taken a fork");
+	sem_wait(data->forks);
+	print_philo_status(philo, "has taken a fork");
+}
+
+void	eating(t_philo *philo)
+{
+	print_philo_status(philo, "is eating");
+	set_time_ms(&(philo->ms_eat_last));
+	if (philo->num_eat < philo->data->num_of_times_each_must_eat)
+	{
+		(philo->num_eat)++;
+		if (philo->num_eat == philo->data->num_of_times_each_must_eat)
+			sem_post(philo->data->full_sem);
+	}
+	sleep_shortly_ms(philo->data->time_to_eat);
+	sem_post(philo->data->forks);
+	sem_post(philo->data->forks);
+}
+
+void	sleeping(t_philo *philo)
+{
+	print_philo_status(philo, "is sleeping");
+	sleep_shortly_ms(philo->data->time_to_sleep);
+}
+
+void	thinking(t_philo *philo)
+{
+	print_philo_status(philo, "is sleeping");
+	usleep(300); // todo: consider
+}
+
 void	process_philo(t_philo *philo)
 {
 	pthread_create(&(philo->monitor_death_thread), NULL, monitor_death_routine, philo);
 	//if (philo->number % 2 == 0)
 	while (TRUE)
 	{
-		//taking_forks(philo);
-		//eating
-		//sleeping
+		taking_forks(philo);
+		eating(philo);
+		sleeping(philo);
 		//thinking
 	}
 }
